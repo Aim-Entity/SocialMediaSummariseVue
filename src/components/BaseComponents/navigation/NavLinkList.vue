ceaf3a15dc5ecbdf5f0025e7ed8d414ceb9ba0d8
BilalUddin <c3071276@hallam.shu.ac.uk>
2024-09-03 14:32:11 +0100

    navigation links completed

--- src/components/BaseComponents/navigation/NavLinkList.vue
+++ src/components/BaseComponents/navigation/NavLinkList.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 
+type linkObject = {
+  link: string;
+  title: string;
+};
+
 const props = withDefaults(
   defineProps<{
     listTitle: string;
     listIconSrc: string;
     active: boolean;
+    links: linkObject[];
   }>(),
   {
-    active: () => false
+    active: () => false,
+    links: () => []
   }
 );
 </script>
@@ -17,25 +24,32 @@ const props = withDefaults(
   <div class="group/appear h-10 content-center">
     <div class="flex gap-1">
       <img :src="listIconSrc" alt="Icon" />
-      <p class="ml-1 text-sm cursor-pointer">{{ listTitle }}</p>
-      <img src="@/assets/icons/down-chev-mute.svg" class="cursor-pointer" alt="Arrow" />
+      <p
+        :class="
+          active ? 'ml-1 text-sm cursor-pointer text-color-green' : 'ml-1 text-sm cursor-pointer'
+        "
+      >
+        {{ listTitle }}
+      </p>
+      <img
+        v-if="active"
+        src="@/assets/icons/down-chev-active.svg"
+        class="cursor-pointer"
+        alt="Arrow"
+      />
+      <img v-else src="@/assets/icons/down-chev-mute.svg" class="cursor-pointer" alt="Arrow" />
     </div>
 
     <div
+      v-if="links.length != 0"
       class="absolute py-6 px-4 bg-white border mt-2 transition transform translate-y-2 ease-in invisible group-hover/appear:visible group-hover/appear:translate-y-0"
     >
       <ul class="flex flex-col gap-3">
-        <li class="flex justify-between gap-10 cursor-pointer group/underline">
-          <p class="text-color-mute group-hover/underline:underline">WhatsApp Summarise</p>
-          <img src="@/assets/icons/right-chev-mute.svg" alt="Arrow" />
-        </li>
-        <li class="flex justify-between gap-10 cursor-pointer group/underline">
-          <p class="text-color-mute group-hover/underline:underline">Text Detection</p>
-          <img src="@/assets/icons/right-chev-mute.svg" alt="Arrow" />
-        </li>
-        <li class="flex justify-between gap-10 cursor-pointer group/underline">
-          <p class="text-color-mute group-hover/underline:underline">Something Else</p>
-          <img src="@/assets/icons/right-chev-mute.svg" alt="Arrow" />
+        <li v-for="(link, index) in links" :key="index">
+          <a :href="link.link" class="flex justify-between gap-10 cursor-pointer group/underline">
+            <p class="text-color-mute group-hover/underline:underline">{{ link.title }}</p>
+            <img src="@/assets/icons/right-chev-mute.svg" alt="Arrow" />
+          </a>
         </li>
       </ul>
     </div>
--- src/components/ExtendedComponents/navigation/BottomNav.vue
+++ src/components/ExtendedComponents/navigation/BottomNav.vue
@@ -4,10 +4,32 @@ import UserInfo from '@/components/BaseComponents/navigation/UserInfo.vue';
 import NavLinkList from '@/components/BaseComponents/navigation/NavLinkList.vue';
 
 import dashboardIconMute from '@/assets/icons/home-mute.svg';
+import appIconMute from '@/assets/icons/apps-active.svg';
+import moreIconMute from '@/assets/icons/more-mute.svg';
+import type { link } from 'fs';
 </script>
 
 <template>
-  <div class="default-page-padding h-12 flex items-center shadow-md w-full">
-    <NavLinkList list-title="Dashboard" :list-icon-src="dashboardIconMute" />
+  <div class="default-page-padding h-12 flex items-center shadow-md w-full gap-8">
+    <NavLinkList
+      list-title="Dashboards"
+      :list-icon-src="dashboardIconMute"
+      :links="[{ link: '#', title: 'Home' }]"
+    />
+    <NavLinkList
+      list-title="Apps"
+      :list-icon-src="appIconMute"
+      :active="true"
+      :links="[
+        { link: '#', title: 'WhatsApp Summary' },
+        { link: '#', title: 'Text Decoration' },
+        { link: '#', title: 'Something Else' }
+      ]"
+    />
+    <NavLinkList
+      list-title="More"
+      :list-icon-src="moreIconMute"
+      :links="[{ link: '#', title: 'Contact' }]"
+    />
   </div>
 </template>
